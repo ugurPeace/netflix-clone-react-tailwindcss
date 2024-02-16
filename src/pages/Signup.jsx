35aaa6936e94252ee177b8f3c3ca9ed0a956ac77
Ugur <ugurarslanpeace@gmail.com>
2024-02-16 19:06:51 +0300

    max h fixed

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
         />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen "></div>
         <div className="fixed w-full px-4 py-24 z-50">
-          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[450px] h-[550px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1>Sign Up</h1>
             </div>
